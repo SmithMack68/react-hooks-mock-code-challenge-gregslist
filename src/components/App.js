d174517fedf15902d39d71239575343ff4593d74
Candice Smith MacKenzie <Mahony8@aol.com>
2022-03-07 00:12:16 -0500

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect, useState }from "react";
 import Header from "./Header";
 import ListingsContainer from "./ListingsContainer";
 
 function App() {
+  const [listings, setListings]= useState([])
+  const [search, setSearch] = useState("")
+  
+  useEffect(() => {
+    fetch("http://localhost:6001/listings")
+      .then(resp => resp.json())
+      .then(setListings)
+  }, [])
+
+function handleRemoveListing(id){
+  const updatedListings = listings.filter(listing => listing.id !== id)
+  setListings(updatedListings)
+}
+
+const displayedListings = listings.filter(listing => 
+  listing.description.toLowerCase().includes(search.toLowerCase()))
+
   return (
     <div className="app">
-      <Header />
-      <ListingsContainer />
+      <Header onSearch={setSearch}/>
+      <ListingsContainer
+      listings={displayedListings} 
+      onRemoveListing={handleRemoveListing}/>
     </div>
   );
 }
